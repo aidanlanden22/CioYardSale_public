1fc1d801f27054b706e3c000acfee18f301e2c9f
Emma <enf5cb@virginia.edu>
2018-02-18 16:06:47 -0500

    models for the students + a view + a signup page

--- students/views.py
+++ students/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+
+from .forms import SignUpUser, SignUpStudent
+from .models import Student
+
 
 def profile(request):
     return render(request, 'students/profile.html')
+
+def signup(request):
+    if request.user.is_authenticated and Student.objects.filter(user=request.user).exists():
+        return HttpResponseRedirect(reverse('index'))
+
+    if request.method == 'POST':
+        userForm = SignUpUser(request.POST)
+        studentForm = SignUpStudent(request.POST)
+        formErrors = {}
+        formErrors.update(userForm.errors)
+        formErrors.update(studentForm.errors)
+        if userForm.is_valid() and studentForm.is_valid():
+            user = User.objects.create_user(
+                username=userForm.cleaned_data['username'],
+                password=userForm.cleaned_data['password'],
+                first_name=userForm.cleaned_data['first_name'],
+                last_name=userForm.cleaned_data['last_name'],
+                email=userForm.cleaned_data['email']
+            )
+            user.save()
+            student = studentForm.save(commit=False)
+            student.user = user
+            student.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse('students:profile'))
+
+        return render(request, 'students/signup.html', {'errors': formErrors,  'userForm': userForm, 'studentForm': studentForm})
+
+    userForm = SignUpUser()
+    studentForm = SignUpStudent()
+    return render(request, 'students/signup.html', {'userForm': userForm, 'studentForm': studentForm})
