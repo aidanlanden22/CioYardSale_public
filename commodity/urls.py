eb67fa0d4965c3b0e9229abeea8d2765645a231e
Emma <enf5cb@virginia.edu>
2018-02-19 18:49:53 -0500

    small changes

--- commodity/urls.py
+++ commodity/urls.py
@@ -5,8 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.sell, name='sell'),
     url(r'^create/$', views.create, name='create'),
-    
-    
     url(r'^(?P<pk>[0-9]+)/$', views.read, name='read'),
-    
 ]
--- commodity/views.py
+++ commodity/views.py
@@ -11,9 +11,9 @@ import json
 # Create your views here.
 def sell(request):
     if request.method == 'POST':
-        
+
         form = CommodityForm(request.POST)
-   
+
         if form.is_valid():
             selling = Commodity.objects.create(
                 g_or_s=form.cleaned_data['g_or_s'],
@@ -24,7 +24,7 @@ def sell(request):
                 title=form.cleaned_data['title']
             )
             selling.save()
-            
+
             return HttpResponseRedirect('/')
 
     # if a GET (or any other method) we'll create a blank form
@@ -32,7 +32,7 @@ def sell(request):
         form = CommodityForm()
 
     return render(request, 'createcommodity.html', {'form': form})
-    
+
 # ********** THIS IS THE API FOR COMMODITIES **********
 def read(request, pk):
     # Format the JSON Response for a GET Request
@@ -42,28 +42,28 @@ def read(request, pk):
 
         data = serializers.serialize("json", Commodity.objects.filter(id=pk))
     return HttpResponse(data, content_type='application/json')
-    
+
 @csrf_exempt
 def create(request):
     #if request.method == 'POST':
-    
-    
+
+
     if request.method == 'POST':
         form = CommodityForm(request.POST)
         print("hey334")
         if form.is_valid():
-            
+
             selling = Commodity.objects.create(
                 g_or_s=form.cleaned_data['g_or_s'],
                 quantity=form.cleaned_data['quantity'],
                 description=form.cleaned_data['description'],
                 price=form.cleaned_data['price'],
                 date_expires=form.cleaned_data['date_expires'],
-                title=form.cleaned_data['title']
+                title=form.cleaned_data['title'],
+                cio=request.user
             )
 
             selling.save()
-            
+
             data = serializers.serialize("json", [selling])
             return HttpResponse(data, content_type='application/json')
-            
