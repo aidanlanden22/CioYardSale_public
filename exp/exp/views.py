dbf67d7f7a1e84b17ec8b2ee7ded7de43ccd7d97
Summer <sst2bd@virginia.edu>
2018-03-12 19:32:22 -0400

    experience view works

--- exp/exp/views.py
+++ exp/exp/views.py
@@ -1,10 +1,11 @@
 import urllib.request
 import urllib.parse
 import json
+from django.http import JsonResponse
 
 def getCommodityList(request):
     req = req = urllib.request.Request('http://models-api:8000/api/v1/commodity/readAll/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp = json.loads(resp_json)
 
-    return JsonResponse(resp)
+    return JsonResponse(resp, safe=False)
--- web/frontend/views.py
+++ web/frontend/views.py
@@ -7,14 +7,5 @@ import urllib.parse
 import json
 
 def index(request):
-    req = urllib.request.Request('http://exp-api:8000/api/v1/experience/getCommodityList/')
-    # json_response = urllib.request.urlopen(req).read().decode('utf-8')
-    # response = json.loads(json_response)
-    # items = json.loads(response)
-
-    context = {
-        'items' : [],
-        'req' : req,
-    }
-
-    return render(request, 'index.html', context)
+    req = urllib.request.Request('http://exp-api:8002/getCommodityList/')
+    json_response = urllib.request.urlopen(req).read().decode('utf-8')
