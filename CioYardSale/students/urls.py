49fb78656a4ebdde48dd049d2632ecf1c1c0b258
Emma <enf5cb@virginia.edu>
2018-03-12 19:04:35 -0400

    took unneccessary things out of students api

--- CioYardSale/students/urls.py
+++ CioYardSale/students/urls.py
@@ -6,11 +6,6 @@ from . import views
 app_name = 'students'
 
 urlpatterns = [
-    url(r'^$', views.profile, name='profile'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^logout/$', views.logoutUser, name='logout'),
-
-    # API URLS
     url(r'^(?P<pk>[0-9]+)/$', views.read, name='read'),
     url(r'^delete/(?P<pk>[0-9]+)/$', views.delete, name='delete'),
     url(r'^update/(?P<pk>[0-9]+)/$', views.update, name='update'),
--- CioYardSale/students/views.py
+++ CioYardSale/students/views.py
@@ -13,47 +13,6 @@ from django.http import JsonResponse
 from django.core import serializers
 import json
 
-def profile(request):
-    myKey = request.user.id
-    return render(request, 'students/profile.html', {'myKey': myKey })
-
-def signup(request):
-    if request.user.is_authenticated and Student.objects.filter(user=request.user).exists():
-        return HttpResponseRedirect(reverse('index'))
-
-    if request.method == 'POST':
-        userForm = SignUpUser(request.POST)
-        studentForm = SignUpStudent(request.POST)
-        formErrors = {}
-        formErrors.update(userForm.errors)
-        formErrors.update(studentForm.errors)
-        if userForm.is_valid() and studentForm.is_valid():
-            user = User.objects.create_user(
-                username=userForm.cleaned_data['username'],
-                password=userForm.cleaned_data['password'],
-                first_name=userForm.cleaned_data['first_name'],
-                last_name=userForm.cleaned_data['last_name'],
-                email=userForm.cleaned_data['email']
-            )
-            user.save()
-            student = studentForm.save(commit=False)
-            student.user = user
-            student.save()
-            login(request, user)
-            return HttpResponseRedirect(reverse('students:profile'))
-
-        return render(request, 'students/signup.html', {'errors': formErrors,  'userForm': userForm, 'studentForm': studentForm})
-
-    userForm = SignUpUser()
-    studentForm = SignUpStudent()
-    return render(request, 'students/signup.html', {'userForm': userForm, 'studentForm': studentForm})
-
-def logoutUser(request):
-    if request.user.is_authenticated():
-        logout(request)
-    return HttpResponseRedirect(reverse('index'))
-
-
 # ********** THIS IS THE API FOR STUDENTS **********
 def read(request, pk):
     data = {}
