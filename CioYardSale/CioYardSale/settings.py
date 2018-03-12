3a3fc10004101d8fa647d5d4f64e7a8ae0b76b51
Emma <enf5cb@virginia.edu>
2018-03-12 18:33:08 -0400

    fix settings

--- CioYardSale/CioYardSale/settings.py
+++ CioYardSale/CioYardSale/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.students',
-    'api.commodity',
-    'api.cio',
+    'students',
+    'commodity',
+    'cio',
     # 'frontend'
 ]
 
