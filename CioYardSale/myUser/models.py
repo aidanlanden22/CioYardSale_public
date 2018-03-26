631e012f5666b6b27573e697b47d2f68949827a8
Summer <sst2bd@virginia.edu>
2018-03-26 13:39:57 -0400

    successful migration of the myUser model

--- CioYardSale/myUser/models.py
+++ CioYardSale/myUser/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class myUser(models.Model):
+    username = models.CharField(max_length=30, unique=True, default=None)
+    password = models.CharField(max_length=100, default=None)
+
+    def __str__(self):
+        return self.username
+
+
+class Authenticater():
+    myUser = models.ForeignKey(myUser)
+    authenticator = models.CharField(max_length=100,primary_key=True)
+    date_created = models.DateTimeField(default=timezone.now)
