ccc627d7801c9e3eb24f26803a5211024a90297f
Summer <sst2bd@virginia.edu>
2018-03-26 17:22:11 +0000

    created app for myUser

--- CioYardSale/students/tests.py
+++ CioYardSale/students/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
 
 # Create your tests here.
+class Tests(TestCase):
+    fixtures = ['database.json']
+
+    #User wants to get a list of commodities
+    def testGetAllCommodities(self):
+        cli = Client()
+        resp = cli.get('/api/v1/commodity/readAll/')
+        commodities = resp.json()
+        self.assertEqual(len(commodities), 2)
