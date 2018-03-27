8ed26dc8f355954d7fb0335459c57cbf58909909
Emma <enf5cb@virginia.edu>
2018-03-26 21:39:02 -0400

    fixed merge conflict

--- CioYardSale/myUser/urls.py
+++ CioYardSale/myUser/urls.py
@@ -7,9 +7,6 @@ app_name = 'myUser'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^create/$', views.create_auth, name='create_auth'),
-<<<<<<< HEAD
     url(r'^create_user/$', views.create_user, name='create_user'),
     url(r'^readAll/$', views.readAll, name='readAll'),
-=======
->>>>>>> 0cd7ddec6249b68ca2462b19dfa6f55b42765e5e
 ]
