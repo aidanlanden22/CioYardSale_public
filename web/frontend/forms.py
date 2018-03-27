182392092887dbf5abc209d9404c1ee6779709aa
Aidan Landen <aidanlanden22@gmail.com>
2018-03-26 22:22:46 -0400

    added createListing to web layer view

--- web/frontend/forms.py
+++ web/frontend/forms.py
@@ -21,3 +21,16 @@ class SignUpUser(forms.ModelForm):
     last_name = forms.CharField(required=True)
     email = forms.CharField(widget=forms.EmailInput, required=True)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+# Form for CIO's to list a new commodity
+class CreateCommodityForm(forms.ModelForm):
+	GOOD_OR_SERVICE = (
+    ('G', 'Good'),
+    ('S', 'Service'),
+    )
+	g_or_s = forms.CharField(max_length=1, choices=GOOD_OR_SERVICE, default='G')
+	title = forms.CharField(required=True)
+	price = forms.DecimalField(max_digits=6, decimal_places=2)
+	description = forms.CharField(required=True)
+	quantity = forms.IntegerField(required=True)
+
