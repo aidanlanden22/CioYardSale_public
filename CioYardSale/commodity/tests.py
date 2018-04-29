d959414387d424b54107268bba712303784d8b7a
Emma <enf5cb@virginia.edu>
2018-04-29 18:57:37 -0400

    try to run unit tests - try commodity tests

--- CioYardSale/commodity/tests.py
+++ CioYardSale/commodity/tests.py
@@ -3,18 +3,18 @@ from django.urls import reverse
 import json
 
 # Create your tests here.
-# class Tests(TestCase):
-#     fixtures = ['database.json']
-#
-#     #User wants to get a list of commodities
-#     def testGetAllCommodities(self):
-#         cli = Client()
-#         resp = cli.get('/api/v1/commodity/readAll/')
-#         commodities = resp.json()
-#         self.assertEqual(len(commodities), 2)
-#
-#     def testGetFirstItem(self):
-#     	cli = Client()
-#     	resp = cli.get('/api/v1/commodity/1/')
-#     	commodity = resp.json()
-#     	self.assertEqual(len(commodity),1)
+class Tests(TestCase):
+    fixtures = ['database.json']
+
+    #User wants to get a list of commodities
+    def testGetAllCommodities(self):
+        cli = Client()
+        resp = cli.get('/api/v1/commodity/readAll/')
+        commodities = resp.json()
+        self.assertEqual(len(commodities), 2)
+
+    def testGetFirstItem(self):
+    	cli = Client()
+    	resp = cli.get('/api/v1/commodity/1/')
+    	commodity = resp.json()
+    	self.assertEqual(len(commodity),1)
