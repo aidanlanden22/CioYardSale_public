0d253ba7d05e6f1a7d51b7bf459dc3649831e5b3
Emma <enf5cb@virginia.edu>
2018-05-07 13:29:31 -0400

    start project 7

--- selenium-tests.py
+++ selenium-tests.py
@@ -1,12 +1,12 @@
 import unittest
 from selenium import webdriver
 
-class PythonTest(unittest.TestCase):
+class WebPageTests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_search_in_python_org(self):
+    def test_web_page(self):
         driver = self.driver
         driver.get("http://127.0.0.1:4444/")
         self.assertIn("CIO Yard Sale", driver.title)
