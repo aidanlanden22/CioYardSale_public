184ca005b959766fac1ca957592468eb591dc7c1
Summer <sst2bd@virginia.edu>
2018-05-07 23:09:38 -0400

    t

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -40,7 +40,7 @@ def view_item(request, pk):
     rec_resp = json.loads(rec_resp_json)
 
     try:
-        lst_recs = len(rec_resp)
+        lst_recs = rec_resp[0]
     except:
         lst_recs = []
     rec_resp_lst = []
